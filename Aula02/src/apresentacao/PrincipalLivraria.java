c3685a0bcd4e3244edeb4613a9028e62a65b82b5
Iago Carvalho <87389959+IagoBGCarvalho@users.noreply.github.com>
2024-08-12 22:48:50 -0300

    Add files via upload

---
+++ Aula02/src/apresentacao/PrincipalLivraria.java
@@ -0,0 +1,35 @@
+package apresentacao;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import negocio.Autor;
+import negocio.Livro;
+
+public class PrincipalLivraria {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+
+        Livro objLivro = new Livro();
+
+        // Entrada de dados
+        try {
+            System.out.print("Digite o título do livro: ");
+            objLivro.setTitulo(leitor.readLine());
+
+            System.out.print("Digite o nome do autor do livro: ");
+            objLivro.setObjAutor(new Autor(leitor.readLine()));
+
+            System.out.print("Digite o gẽnero do livro: ");
+            objLivro.setGenero(leitor.readLine());
+        } catch (Exception erro) {}
+
+        // Saída de dados
+        System.out.println("Nome do livro: " + objLivro.getTitulo());
+        System.out.println("Autor do livro: " + objLivro.getObjAutor().getNome());
+        System.out.println("Gẽnero do livro: " + objLivro.getGenero());
+
+    }
+
+}
